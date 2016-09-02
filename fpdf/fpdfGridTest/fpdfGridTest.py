c4506086169a43177d96eab7a9870daffba16d76
Johan Borgström <johan@petfactory.se>
2016-09-02 15:05:08 +0200

    use continous seq to generate content

--- fpdf/fpdfGridTest/fpdfGridTest.py
+++ fpdf/fpdfGridTest/fpdfGridTest.py
@@ -1,27 +1,19 @@
 from fpdf import FPDF
 from datetime import date
+import pprint
 
 class PDF(FPDF):
-    def header(self):
-        # Logo
-        # self.image('mario.png', 10, 8, 33)
-        # Arial bold 15
-        self.set_font('Arial', 'B', 14)
-        # Move to the right
-        #self.cell(80)
-        # Title
-        #self.cell(0, 10, , 0, 0, 'C')
-        pdf.cell(w=0, h=0, txt='{} - {}'.format(main_title, date.today().isoformat()), border=0, ln=0,  align='C', fill=False, link='')
-        # Line break
-        #self.ln(10)
 
-    # Page footer
+    def header(self):
+
+        # self.image('mario.png', 10, 8, 33)
+        self.set_font('Arial', 'B', 14)
+        pdf.cell(w=0, h=0, txt='{} - {}'.format(main_title, date.today().isoformat()), border=0, ln=0,  align='C', fill=False, link='')
+
     def footer(self):
-        # Position at 1.5 cm from bottom
+
         self.set_y(-15)
-        # Arial italic 8
         self.set_font('Arial', 'I', 7)
-        # Page number
         self.cell(0, 10, 'page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
 
 
@@ -33,40 +25,40 @@ pdf.alias_nb_pages()
 
 
 
-
-# Instantiation of inherited class
-# 842 x 595
-
-
 total_width = 595
 total_height = 842
 
 grid_total_width = 580.0
 grid_total_height = 790.0
 
-num_grid_x = 3
-num_grid_y = 4
-
+grid_num_cols = 3
+grid_num_rows = 4
 
 grid_offset_x = (total_width - grid_total_width) *.5
 grid_offset_y = 30
 
-grid_width = grid_total_width / float(num_grid_x)
-grid_height = grid_total_height / float(num_grid_y)
+def build_placement_grid(total_width, total_height, grid_total_width, grid_total_height, grid_num_cols, grid_num_rows, grid_offset_x, grid_offset_y):
 
-col_list = []
-for col in range(num_grid_x):
-    col_list.append((col * grid_width + grid_width*.5) + grid_offset_x)
+    
+    grid_width = grid_total_width / float(grid_num_cols)
+    grid_height = grid_total_height / float(grid_num_rows)
 
-row_list = []
-for row in range(num_grid_y):
-    row_list.append((row * grid_height + grid_height*.5) + grid_offset_y)
+    row_list = []
+    col_list = []
+
+    for row_index in range(grid_num_rows):
+
+        row_list.append((row_index * grid_height + grid_height*.5) + grid_offset_y)
+
+    for col_index in range(grid_num_cols):
+        col_list.append((col_index * grid_width + grid_width*.5) + grid_offset_x)
 
 
-def buildPage(pdf):
+    return (tuple(row_list), tuple(col_list))
 
 
-    pdf.add_page()
+def buildPage(name_list, row_list, col_list, pdf):
+
     pdf.set_font('Times', '', 12)
 
     #pdf.rect(x=grid_offset_x, y=grid_offset_y, w=grid_total_width, h=grid_total_height, style = '')
@@ -74,26 +66,35 @@ def buildPage(pdf):
     img_width = 160.0
     size = 10
 
-    index = 0
-    for row in row_list:
+    for index, name in enumerate(name_list):
+        #print index, name
+        row = index/(grid_num_rows-1)
+        col = index%(grid_num_cols)
 
-        for col in col_list:
-            #print row, col
-            index += 1
+        print row, col
+        x = col_list[col]
+        y = row_list[row]
 
-            pdf.image('mario_300_frame.png', x=col-img_width*.5, y=row-img_width*.5, w=img_width, h=img_width)
+        pdf.image('mario_300_frame.png', x=x-img_width*.5, y=y-img_width*.5, w=img_width, h=img_width)
 
-            pdf.rect(x=col-size*.5, y=row-size*.5, w=size, h=size, style = 'F')
+        pdf.rect(x=x-size*.5, y=y-size*.5, w=size, h=size, style = 'F')
 
-            pdf.text(x=col-img_width*.5, y=row+14+img_width*.5, txt='{} Material name'.format(index))
+        pdf.text(x=x-img_width*.5, y=y+14+img_width*.5, txt='{} {}'.format(index, name))
 
 
+# Instantiation of inherited class
+# 842 x 595
 
-for mat in range(40):
-    if mat % (num_grid_x*num_grid_y) == 0:
-        print 'new'
-        buildPage(pdf)
+row_list, col_list = build_placement_grid(total_width, total_height, grid_total_width, grid_total_height, grid_num_cols, grid_num_rows, grid_offset_x, grid_offset_y)
 
+seq = [c for c in 'abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQR']
+chunk_length = 12
+chunk_list = [seq[x:x+chunk_length] for x in xrange(0, len(seq), chunk_length)]
+
+for chunk in chunk_list:
+
+    pdf.add_page()
+    buildPage(chunk, row_list, col_list, pdf)
 
 
 pdf.output('mario.pdf', 'F')
